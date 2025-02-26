96f495b6c512d42339ff9b6aad5dd60a26653258
seva02-3 <sevarus18@gmail.com>
2025-02-27 01:29:24 +0400

    Add files via upload

---
+++ DZ123/DZ123/CarsService.cs
@@ -0,0 +1,69 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace DZ123;
+
+public class CarsService
+{
+    private string connectionString = "Data Source=localhost;Initial Catalog=CarsDB;Integrated Security=True;TrustServerCertificate=True";
+
+    public void AddCar(Car car)
+    {
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            var sql = "INSERT INTO Cars (Brand, Model, Year, Price) VALUES (@Brand, @Model, @Year, @Price)";
+            connection.Execute(sql, new { car.Brand, car.Model, car.Year, car.Price });
+        }
+    }
+
+    public void UpdateCar(int carID, decimal price)
+    {
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            var sql = "UPDATE Cars SET Price = @Price WHERE Id = @Id";
+            connection.Execute(sql, new { Id = carID, Price = price });
+        }
+    }
+
+    public void DeleteCar(int carID)
+    {
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            var sql = "DELETE FROM Cars WHERE Id = @carID";
+            connection.Execute(sql, new { carID }); 
+        }
+    }
+
+    public void PrintCars()
+    {
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            var sql = "SELECT * FROM Cars";
+            var cars = connection.Query<Car>(sql);
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Id}: {car.Brand} {car.Model} {car.Year} {car.Price}$");
+            }
+        }
+    }
+
+    public void PrintCarsByBrand(string brand)
+    {
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            var sql = "SELECT * FROM Cars WHERE Brand = Brand";
+            var cars = connection.Query<Car>(sql);
+            
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Id}: {car.Brand} {car.Model} {car.Year} {car.Price}$");
+            }
+        }
+    }
+}
---
+++ DZ123/DZ123/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+using DZ123;
+using Microsoft.Data.SqlClient;
+
+
+class Program
+{
+    private static string connectionString = "Data Source=localhost;Initial Catalog=CarsDB;Integrated Security=True;TrustServerCertificate=True";
+    
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        
+        CarsService carsService = new CarsService();
+        
+        while (true)
+        {
+            Console.WriteLine("\nВыберите действие:");
+            Console.WriteLine("1 - Добавить машину");
+            Console.WriteLine("2 - Обновить цену машины");
+            Console.WriteLine("3 - Удалить машину");
+            Console.WriteLine("4 - Вывести всех машин");
+            Console.WriteLine("5 - Вывести список машин по бренду");
+            Console.WriteLine("6 - Выйти");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Введите бренд: ");
+                    string brand = Console.ReadLine();
+
+                    Console.Write("Введите модель: ");
+                    string model = Console.ReadLine();
+
+                    Console.Write("Введите год выпуска: ");
+                    int year = int.Parse(Console.ReadLine());
+
+                    Console.Write("Введите цену: ");
+                    decimal price = decimal.Parse(Console.ReadLine());
+
+                    carsService.AddCar(new Car { Brand = brand, Model = model, Year = year, Price = price });
+                    break;
+
+                case "2":
+                    Console.Write("Введите ID машины: ");
+                    int carUpdate = int.Parse(Console.ReadLine());
+
+                    Console.Write("Введите новую цену: ");
+                    decimal newPrice = decimal.Parse(Console.ReadLine());
+
+                    carsService.UpdateCar(carUpdate, newPrice);
+                    break;
+
+                case "3":
+                    Console.Write("Введите ID машины для удаления: ");
+                    int carDelete = int.Parse(Console.ReadLine());
+
+                    carsService.DeleteCar(carDelete);
+                    break;
+
+                case "4":
+                    carsService.PrintCars();
+                    break;
+
+                case "5":
+                    Console.Write("Введите бренд: ");
+                    string brandf = Console.ReadLine();
+                    
+                    carsService.PrintCarsByBrand(brandf);
+                    break;
+
+                case "6":
+                    Console.WriteLine("Выход");
+                    return;
+            }
+        }
+    }
+}
